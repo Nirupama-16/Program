50af4ed8387a7d1ecc6a5b84c643e1e996a5eb9e
Nirupama-16 <91303818+Nirupama-16@users.noreply.github.com>
2021-12-02 10:53:49 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ITLab1-20
+ */
+public class Add 
+{
+    public static void main(String args[])
+    {
+     int a=10,b=20,c;
+     c=a+b;
+     System.out.println("Addition 0f two numbers is:"+c);
+    }
+}
